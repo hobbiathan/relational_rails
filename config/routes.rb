209d4fb5100c5b9ae5c39d94f0a9ece2d6de102b
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-29 19:04:57 -0700

    add essential routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
+
+  # Index (User story 1)
+  get '/wargames', to: 'wargames#index'
+  get '/boardgames', to: 'boardgames#index'
+
+  # Specific parent in index
+  get '/wargames/:id', to: 'wargames#show'
+  get '/boardgames/:id', to: 'boardgames#show'
+
+  get '/warrooms', to: 'warrooms#index'
+  get '/boardgames', to: 'boardgames#index'
+
+  get '/wargames/:id', to: 'wargames#show'
+  get '/boardgames/:id', to: 'boardgames#show'
+
+  get '/wargames/:id/warrooms', to: 'wargames#index'
+  get '/boardgames/:id/boardgames', to: 'boardgames#index'
+
 end
