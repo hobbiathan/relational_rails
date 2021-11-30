bafd088bc75a0ec11594418b9ce0ff64031a6a73
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-29 19:48:49 -0700

    Create show method

--- app/controllers/wargames_controller.rb
+++ app/controllers/wargames_controller.rb
@@ -2,4 +2,8 @@ class WargamesController < ApplicationController
   def index
     @countries = ["United States of America", "Russian Federation", "People's Republic of China"]
   end
+
+  def show
+
+  end
 end
