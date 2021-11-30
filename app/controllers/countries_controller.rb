475cbfd05697c331976f5a99c31853c7d9e7e15f
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-29 20:18:13 -0700

    rename wargames_controller.rb -> countries_controller.rb

---
+++ app/controllers/countries_controller.rb
@@ -0,0 +1,9 @@
+class CountriesController < ApplicationController
+  def index
+    @countries = ["United States of America", "Russian Federation", "People's Republic of China"]
+  end
+
+  def show
+
+  end
+end
