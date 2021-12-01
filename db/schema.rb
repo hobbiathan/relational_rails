64e4ad35e073a9736ea0101fd5b7565690560604
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-30 19:07:50 -0700

    rails db:migrate

---
+++ db/schema.rb
@@ -0,0 +1,26 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_12_01_020704) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "countries", force: :cascade do |t|
+    t.integer "military_power_rank"
+    t.string "country_name"
+    t.boolean "nuclear_power"
+    t.date "created_at"
+    t.date "updated_at"
+  end
+
+end
