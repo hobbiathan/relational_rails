0c728146e3547e3d86203abf16a7a8787c3541dd
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-02 05:40:01 -0700

    Warrooms db:migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_020704) do
+ActiveRecord::Schema.define(version: 2021_12_02_123853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,16 @@ ActiveRecord::Schema.define(version: 2021_12_01_020704) do
     t.date "updated_at"
   end
 
+  create_table "warrooms", force: :cascade do |t|
+    t.integer "strategic_importance"
+    t.string "warroom_name"
+    t.boolean "deadman_switch"
+    t.boolean "contains_wmd"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "country_id"
+    t.index ["country_id"], name: "index_warrooms_on_country_id"
+  end
+
+  add_foreign_key "warrooms", "countries"
 end
