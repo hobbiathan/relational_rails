69b3bbcca1c832f953623353528eab67230a356b
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-02 13:46:42 -0700

    Create #self.desc_order, #child_count

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,11 @@
 class Country < ApplicationRecord
   has_many :warrooms
+
+  def self.desc_order
+    order(created_at: :desc)
+  end
+
+  def child_count
+    Warroom.where(country_id: self.id).count
+  end
 end
