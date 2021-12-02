8ddc77b2cd8cf351b2593d4bed33dffc81caac7e
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-02 06:36:23 -0700

    Create #warrooms (not dry)

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,6 +4,20 @@ class CountriesController < ApplicationController
   end
 
   def show
+    #binding.pry
     @current_country = Country.find(params[:id])
+    #binding.pry
+    @warrooms = @current_country.warrooms
+    #binding.pry
+  end
+
+# Not dry - how to utilize show method for nested page?
+  def warrooms
+    @current_country = Country.find(params[:id])
+    @warrooms = @current_country.warrooms
+  end
+
+  def child_warroom
+
   end
 end
