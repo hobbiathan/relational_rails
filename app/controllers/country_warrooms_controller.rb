a1ce1aef2efe29d079da702a110df11175568e06
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-04 17:48:47 -0700

    Create #show

--- app/controllers/country_warrooms_controller.rb
+++ app/controllers/country_warrooms_controller.rb
@@ -4,4 +4,10 @@ class CountryWarroomsController < ApplicationController
     @warrooms = @current_country.warrooms
   end
 
+  def show
+    #binding.pry
+    @current_country = Country.find(params[:id])
+    @warroom = Warroom.find(params[:warroom_id])
+    #binding.pry
+  end
 end
