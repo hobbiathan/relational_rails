871b49c0e4e8de4cd06b8b411fa5683321fde07b
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-04 22:57:05 -0700

    refactor, add test for user story 11

--- spec/features/countries/index_spec.rb
+++ spec/features/countries/index_spec.rb
@@ -5,20 +5,28 @@ RSpec.describe 'countries' do
   before(:each) do
     @country = Country.create!(military_power_rank: 5, country_name: "The Velviathan", nuclear_power: true)
     @country_two = Country.create!(military_power_rank: 4, country_name: "The State of Hobbiathan", nuclear_power: true)
-  end
-
-  it 'Lists and links to all Countries' do
 
     visit "/countries"
+  end
 
-    #save_and_open_page
+  it 'Lists all countries' do
 
     expect(page).to have_content(@country.country_name)
     expect(page).to have_content(@country_two.country_name)
+
+  end
+
+  it 'links to respective countries' do
     expect(page).to have_link("#{@country.country_name}", :href => "/countries/#{@country.id}")
     expect(page).to have_link("#{@country_two.country_name}", :href => "/countries/#{@country_two.id}")
-    expect(page).to have_link("All Warrooms", :href => "/warrooms")
+  end
 
+  it 'links to all warrooms' do
+    expect(page).to have_link("All Warrooms", :href => "/warrooms")
+  end
+
+  it 'contains link to create new parent' do
+    expect(page).to have_link("New Parent", :href => "/countries/new")
   end
 
 end
