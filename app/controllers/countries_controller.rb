adc838198c6530f1f3287a708f2855d0ea0426a4
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-04 17:54:09 -0700

    Remove improper code

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -12,23 +12,4 @@ class CountriesController < ApplicationController
     #binding.pry
   end
 
-# # Not dry - how to utilize show method for nested page?
-#   def warrooms
-#     @current_country = Country.find(params[:id])
-#     @warrooms = @current_country.warrooms
-#   end
-#
-#   # same thing as above
-#   # does this follow that one thing that says information shouldn't be accessible
-#   # from foreign parents?
-#
-#   # probably not
-#
-#
-#   def child_warroom
-#     #binding.pry
-#     @current_country = Country.find(params[:id])
-#     @warroom = Warroom.find(params[:warroom_id])
-#     #binding.pry
-#   end
 end
