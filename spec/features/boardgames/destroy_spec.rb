067e812e34b9ce51642fdebdaa5fda6b25b32d61
Darren Kulback <dkulback@gmail.com>
2021-12-05 16:38:46 -0700

    add destroy test

---
+++ spec/features/boardgames/destroy_spec.rb
@@ -0,0 +1,24 @@
+# User Story 20, Child Delete (x2)
+#
+# As a visitor
+# When I visit a child show page
+# Then I see a link to delete the child "Delete Child"
+# When I click the link
+# Then a 'DELETE' request is sent to '/child_table_name/:id',
+# the child is deleted,
+# and I am redirected to the child index page where I no longer see this child
+
+require 'rails_helper'
+
+RSpec.describe 'destroying an boardgame' do
+  it 'can delete the artist from the index page' do
+    emperor = Company.create!(name: "Emperors of eternal evil")
+    cave_evil = emperor.boardgames.create!(name: "Cave Evil", rank: 10, out_of_print: true)
+    visit "/boardgames/#{cave_evil.id}"
+
+    
+    click_button 'Delete Board Game'
+    expect(current_path).to eq('/boardgames')
+    expect(page).to_not have_content(cave_evil.name)
+  end
+end
