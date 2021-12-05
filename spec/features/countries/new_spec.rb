77163dd61be6e105b618135049af5173917f8256
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-04 23:34:30 -0700

    rename

--- spec/features/countries/new_spec.rb
+++ spec/features/countries/new_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe 'countries#new' do
   it 'shows proper field inputs' do
     visit "/countries/new"
 
-    expect(page).to have_field("CountryName")
-    expect(page).to have_field("MilitaryPowerRank")
-    expect(page).to have_field("NuclearPower")
+    expect(page).to have_field("country_name")
+    expect(page).to have_field("military_power_rank")
+    expect(page).to have_field("nuclear_power")
   end
 end
