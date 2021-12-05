15399d0982d03926cf7bf419fc4afa462679e281
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-04 23:17:57 -0700

    add test tests

--- spec/features/countries/new_spec.rb
+++ spec/features/countries/new_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe 'countries' do
-  before(:each) do
-    @country = Country.create!(military_power_rank: 5, country_name: "The Velviathan", nuclear_power: true)
-    @country_two = Country.create!(military_power_rank: 4, country_name: "Enoi", nuclear_power: true)
-  end
+RSpec.describe 'countries#new' do
 
-  
+  it 'shows proper field inputs' do
+    visit "/countries/new"
+
+    expect(page).to have_field("CountryName")
+    expect(page).to have_field("StrategicImportance")
+    expect(page).to have_filed("DeadmanSwitch")
+    expect(page).to have_field("ContainsWMD")
+  end
 end
