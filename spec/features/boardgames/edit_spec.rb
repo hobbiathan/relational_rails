22112b18c139d0518e49d8ba217c804c47ca2254
Darren Kulback <dkulback@gmail.com>
2021-12-06 13:24:51 -0700

    fix within class association

--- spec/features/boardgames/edit_spec.rb
+++ spec/features/boardgames/edit_spec.rb
@@ -1,21 +1,10 @@
-# User Story 14, Child Update (x2)
-#
-# As a visitor
-# When I visit a Child Show page
-# Then I see a link to update that Child "Update Child"
-# When I click the link
-# I am taken to '/child_table_name/:id/edit' where I see a form to edit the child's attributes:
-# When I click the button to submit the form "Update Child"
-# Then a `PATCH` request is sent to '/child_table_name/:id',
-# the child's data is updated,
-# and I am redirected to the Child Show page where I see the Child's updated information
 require 'rails_helper'
 
 RSpec.describe 'Update boardgame with new Data' do
   it 'links to the edit page' do
     @riogrande = Company.create!(name: "Rio Grande", games_invented: 3, independent: false)
     @puerto = @riogrande.boardgames.create!(name: 'Puerto Rico', rank: 13, out_of_print: false)
-    
+
     visit "/boardgames/#{@puerto.id}"
 
 
@@ -34,16 +23,16 @@ RSpec.describe 'Update boardgame with new Data' do
     expect(page).to have_content(false)
     click_link "Update Board Game"
 
-    within '.update-boardgame'
+    within '.update-boardgame' do
       fill_in :name, with: 'Puerto Rico'
       fill_in :rank, with: 110
       select(:false, from: :out_of_print )
       click_button 'Submit Update'
-
+    end
     expect(current_path).to eq("/boardgames/#{@puerto.id}")
-    expect(page).to have_content("Puerto Rico")
+
+    expect(page).to have_content(@puerto.name)
     expect(page).to have_content(110)
     expect(page).to have_content(false)
-
   end
 end
