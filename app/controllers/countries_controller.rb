f5558eebe838246f09baed57211e49e58883e494
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-05 19:27:33 -0700

    fix some issues with the way #create was structured

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -12,8 +12,24 @@ class CountriesController < ApplicationController
   end
 
   def create
+    #binding.pry
+    #binding.pry
+    nuclear_conversion = nil
 
+    if params[:nuclear_power] == "true"
+      nuclear_conversion = true
+    end
+
+    if params[:nuclear_power] == "false"
+      nuclear_conversion = false
+    end
+
+    country = Country.create({
+      country_name: params[:country_name],
+      nuclear_power: nuclear_conversion,
+      military_power_rank: params[:military_power_rank]
+      })
+
+    redirect_to '/countries'
   end
-
-
 end
