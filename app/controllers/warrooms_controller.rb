20ddac6759b8e9ca0cdd5c034e433628b5d2a09f
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-06 15:53:21 -0700

    Create #update, #warroom_params

--- app/controllers/warrooms_controller.rb
+++ app/controllers/warrooms_controller.rb
@@ -10,4 +10,16 @@ class WarroomsController < ApplicationController
   def edit
     @current_warroom = Warroom.find(params[:id])
   end
+
+  def update
+    #binding.pry
+    current_warroom = Warroom.find(params[:id])
+    current_warroom.update(warroom_params)
+
+    redirect_to "/warrooms/#{current_warroom.id}"
+  end
+
+  def warroom_params
+    params.permit(:warroom_name, :strategic_importance, :deadman_switch, :contains_wmd)
+  end
 end
