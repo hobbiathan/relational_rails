b87ee134e3ed7c505d4d9629d2634d6716ac8fc9
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-06 15:10:30 -0700

    create and update #new, #create

--- app/controllers/country_warrooms_controller.rb
+++ app/controllers/country_warrooms_controller.rb
@@ -10,4 +10,20 @@ class CountryWarroomsController < ApplicationController
     @warroom = Warroom.find(params[:warroom_id])
     #binding.pry
   end
+
+  def new
+    #binding.pry
+    @current_country = Country.find(params[:id])
+  end
+
+  def create
+    current_country = Country.find(params[:id])
+    country_warroom = current_country.warrooms.create(warroom_name: params[:warroom_name], strategic_importance: params[:strategic_importance], deadman_switch: params[:deadman_switch], contains_wmd: params[:contains_wmd])
+
+    redirect_to "/countries/#{current_country.id}/warrooms"
+  end
+
+  def country_warroom_params
+    params.permit(:warroom_name, :strategic_importance, :deadman_switch, :contains_wmd)
+  end
 end
