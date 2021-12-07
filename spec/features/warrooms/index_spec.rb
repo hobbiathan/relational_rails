2f49928d76f7fb378bdcab6a6f144bd933c81c82
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-07 14:59:50 -0700

    make tests

--- spec/features/warrooms/index_spec.rb
+++ spec/features/warrooms/index_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'warrooms' do
     @country_two = Country.create!(military_power_rank: 4, country_name: "The State of Hobbiathan", nuclear_power: true)
     @wr = @country.warrooms.create!(strategic_importance: 5, warroom_name: "Sector 2B", deadman_switch: true, contains_wmd: true)
     @wr_2 = @country_two.warrooms.create!(strategic_importance: 4, warroom_name: "Khoslin", deadman_switch: false, contains_wmd: true)
+    @wr_3 = @country_two.warrooms.create!(strategic_importance: 4, warroom_name: "Mostina", deadman_switch: false, contains_wmd: false)
+
   end
 
   it 'can display proper links' do
@@ -35,6 +37,21 @@ RSpec.describe 'warrooms' do
     expect(page).to have_content(@wr_2.updated_at)
     expect(page).to have_content(@wr_2.country_id)
 
+  end
+
+  it 'can display only warrooms that contain wmds' do
+    visit '/warrooms'
+
+    expect(page).to_not have_content('Mostina')
+  end
+
+  it 'can organize by name' do
+    visit '/warrooms'
+
+    click_link("Order by Name")
+
+    expect(page).to have_content("Khoslin")
+    expect(page).to have_content("Sector 2B")
   end
 
 end
