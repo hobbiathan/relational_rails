a5865e3d8ad8d0318856e55940a777bda6cc7496
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-06 17:43:08 -0700

    add additional tests for coverage

--- spec/features/countries/warrooms/new_spec.rb
+++ spec/features/countries/warrooms/new_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe 'country_warrooms#new' do
   before(:each) do
     @country = Country.create!(military_power_rank: 5, country_name: "The Velviathan", nuclear_power: true)
+    @country_2 = Country.create!(military_power_rank: 5, country_name: "The Hobbiathan", nuclear_power: true)
+
   end
 
   it 'links to /new from /countries/:id' do
@@ -28,6 +30,22 @@ RSpec.describe 'country_warrooms#new' do
     expect(page).to have_content("5")
     expect(page).to have_content("true")
     expect(page).to have_content("false")
+
+    visit "/countries/#{@country_2.id}/warrooms/new"
+
+    fill_in(:warroom_name, with: "Sector 2A")
+    fill_in(:strategic_importance, with: '3')
+    fill_in(:deadman_switch, with: "false")
+    fill_in(:contains_wmd, with: "true")
+
+    click_button('Create Warroom')
+
+    expect(current_path).to eq("/countries/#{@country_2.id}/warrooms")
+    expect(page).to have_content("Sector 2A")
+    expect(page).to have_content("3")
+    expect(page).to have_content("false")
+    expect(page).to have_content("true")
+
 
     #save_and_open_page
   end
